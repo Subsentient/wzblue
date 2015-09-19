4a9a21262a547b82aa1b7c2519d011fccfbec129
Subsentient <thinkingrodent@gmail.com>
2015-09-19 03:48:42 -0700

    Fix Windows version reporting and update copyright

--- src/wzblue.h
+++ src/wzblue.h
@@ -1,7 +1,7 @@
 /*
 WZBlue is a standalone version of aqu4bot's wz.c Warzone 2100 lobby facilities and aqu4bot's networking core.
 See http://github.com/Subsentient/aqu4bot for aqu4bot's source code.
-Public domain. By Subsentient, 2014.
+Public domain. By Subsentient, 2015.
 */
 
 #ifndef __WZBLUE_H__
