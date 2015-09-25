b90386012bced5540b1a4e6510b62d67bb3f1efe
Subsentient <thinkingrodent@gmail.com>
2015-09-24 20:10:21 -0700

    Give tmp credit for the icons

--- src/gui.c
+++ src/gui.c
@@ -149,7 +149,7 @@ void GUI_DrawAboutDialog()
 	
 	snprintf(AboutString, sizeof AboutString, "WZBlue Warzone 2100 Lobby Monitor version " WZBLUE_VERSION "\n"
 										"Compiled against GTK %d.%d\n\n"
-										"By Subsentient.", GTK_MAJOR_VERSION, GTK_MINOR_VERSION);
+										"By Subsentient. Game status icons by tmp500.", GTK_MAJOR_VERSION, GTK_MINOR_VERSION);
 	
 	GtkWidget *Button = gtk_button_new_from_stock(GTK_STOCK_OK);
 	GtkWidget *Label = gtk_label_new(AboutString);
