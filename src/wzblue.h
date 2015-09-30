10218a7a4839ac9df25c7045de4cfd87b5a85be5
Subsentient <thinkingrodent@gmail.com>
2015-09-30 07:33:45 -0700

    We're using a 3-digit version system now.

--- src/wzblue.h
+++ src/wzblue.h
@@ -8,7 +8,7 @@ See the included file UNLICENSE.TXT for more information.
 #ifndef __WZBLUE_H__
 #define __WZBLUE_H__
 
-#define WZBLUE_VERSION "1.1"
+#define WZBLUE_VERSION "1.1.0"
 
 #include <gtk/gtk.h>
 
